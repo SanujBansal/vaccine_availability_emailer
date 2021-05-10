444628b6d10437d1ff5de0d7f32a5f989f874cf9
sanujbansal <sanuj1770.cse16@chitkara.edu.in>
2021-05-10 20:47:15 +0530

    improvements and bug fixes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer-core");
 var nodemailer = require('nodemailer');
 const promptly = require('promptly');
 
+const notifier = require('node-notifier');
+
 const kaam_k_center = [];
 var receivers = [];
 var transporter = nodemailer.createTransport({
@@ -12,13 +14,7 @@ var transporter = nodemailer.createTransport({
        }
    });
 
-   const mailOptions = {
-    from: 'Notification Service covid@notification.com', // sender address
-    to: receivers, // list of receivers
-    subject: '18+ vaccination available book now', // Subject line
-    html: "Hello This is email from server running at sanuj's pc to tell that 18+ vaccine is now available" // plain text body
-  };
-var mobile = "";
+var mobileNumber = "";
 const script = async () => {
     const browser = await puppeteer.launch({
         executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
@@ -35,6 +31,12 @@ const script = async () => {
     await promptly.prompt("enter list of emails seperated by spaces").then((input) => {
         receivers = input.split(" ");
     });
+    const mailOptions = {
+        from: 'Notification Service covid@notification.com', // sender address
+        to: receivers, // list of receivers
+        subject: '18+ vaccination available book now', // Subject line
+        html: "Hello This is email from server running at sanuj's pc to tell that 18+ vaccine is now available" // plain text body
+      };
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     await page.setViewport({ height: 600, width: 800 });
@@ -54,37 +56,38 @@ const script = async () => {
     const pincode = await promptly.prompt('Pincode: ', { validator });
     await page.type('#mat-input-2', pincode, { delay: 20 });
     await page.click('#main-content > app-appointment-table > ion-content > div > div > ion-grid > ion-row > ion-grid > ion-row > ion-col > ion-grid > ion-row > ion-col:nth-child(2) > form > ion-grid > ion-row > ion-col.col-padding.ion-text-start.ng-star-inserted.md.hydrated > ion-button');
+    console.log('Server started successfully, lets wait for the notification on your device and email');
     await page.on('response', (response) => {
         if (response._url.includes('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin')) {
             response.json().then((res) => {
                 res.centers.forEach( (center) => {
-                    var mila = false;
                     center.sessions.forEach(session => {
                         if (session.available_capacity > 0 && session.min_age_limit == 18) {
-                            kaam_k_center.push(session);
-                            console.log(kaam_k_center);
+                            kaam_k_center.push(center);
                         }
                     });  
                 } );
                 if (kaam_k_center.length > 0) {
+                    console.log(kaam_k_center);
+                    notifier.notify('Book slot for covid vaccine now');
                     transporter.sendMail(mailOptions, function (err, info) {
+                        setTimeout(() => {
+                            page.click('#main-content > app-appointment-table > ion-content > div > div > ion-grid > ion-row > ion-grid > ion-row > ion-col > ion-grid > ion-row > ion-col:nth-child(2) > form > ion-grid > ion-row > ion-col.col-padding.ion-text-start.ng-star-inserted.md.hydrated > ion-button');
+                        }, 2000000) 
                         if(err)
                           console.log(err)
                         else
                           console.log(info);
                      });
+                } else {
+                    setTimeout(() => {
+                        page.click('#main-content > app-appointment-table > ion-content > div > div > ion-grid > ion-row > ion-grid > ion-row > ion-col > ion-grid > ion-row > ion-col:nth-child(2) > form > ion-grid > ion-row > ion-col.col-padding.ion-text-start.ng-star-inserted.md.hydrated > ion-button');
+                    }, 20000)
                 }
-                setTimeout(() => {
-                    page.click('#main-content > app-appointment-table > ion-content > div > div > ion-grid > ion-row > ion-grid > ion-row > ion-col > ion-grid > ion-row > ion-col:nth-child(2) > form > ion-grid > ion-row > ion-col.col-padding.ion-text-start.ng-star-inserted.md.hydrated > ion-button');
-                }, 200000)
             }).catch(err => {
             });
         }
     })
-    // const response = await page.waitForResponse((request) => 
-    //     request._url === 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=110034&date=09-05-2021'
-    //     , { timeout: 200000});
-    // console.log(await response.json());
 }
 
 
